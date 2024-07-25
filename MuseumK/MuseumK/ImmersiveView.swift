d3c5af640d752a88d3daa34d1d7a98f0b8e0d3a6
seoyounghan <happyunbd364@naver.com>
2024-07-25 14:34:25 +0900

    spatial 폴더 추가

---
+++ MuseumK/MuseumK/ImmersiveView.swift
@@ -0,0 +1,25 @@
+//
+//  ImmersiveView.swift
+//  MuseumK
+//
+//  Created by hanseoyoung on 7/25/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(scene)
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .mixed) {
+    ImmersiveView()
+}
